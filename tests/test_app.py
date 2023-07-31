27171ca1d80280fe7ab6e56e85a3759d20cd5dc7
Alexandre Reis de Moura <alexandre.moura@ativossa.com.br>
2023-07-31 17:36:13 -0300

    atualizando endpoints para usar banco de dados

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
+from fast_zero.schemas import UserPublic
 
 client = TestClient(app)
 
@@ -11,38 +12,36 @@ def test_root_deve_retornar_200_e_ola_mundo():
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_create_user():
+def test_create_user(client):
     response = client.post(
         '/users/',
         json={
-            'username': 'xandao',
-            'email': 'reialexandre@gmail.com',
+            'username': 'alice',
+            'email': 'alice@example.com',
             'password': 'secret',
         },
     )
     assert response.status_code == 201
     assert response.json() == {
-        'username': 'xandao',
-        'email': 'reialexandre@gmail.com',
+        'username': 'alice',
+        'email': 'alice@example.com',
         'id': 1,
     }
 
 
-def test_read_users():
+def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'xandao',
-                'email': 'reialexandre@gmail.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user():
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -59,7 +58,7 @@ def test_update_user():
     }
 
 
-def test_delete_user():
+def test_delete_user(client, user):
     response = client.delete('/users/1')
     assert response.status_code == 200
     assert response.json() == {'detail': 'User deleted'}
